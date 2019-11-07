e74d8814d883b19ec6ae0d39c54a21b86d51bb61
Ravi <ravi.maurya01234@gmail.com>
2019-11-07 18:34:02 +0530

    Day 49

---
+++ Struts2/gradle-app/src/main/java/com/techlabs/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.techlabs.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeController
+ */
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public HomeController() {
+        super();
+
+        System.out.println("Inside home controller");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Inside home GET");
+		
+		RequestDispatcher view = request.getRequestDispatcher("home.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Inside home POST");
+		doGet(request, response);
+	}
+
+}
